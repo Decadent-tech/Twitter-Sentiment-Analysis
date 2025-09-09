40234407f64e9a160da08383beada55362860896
Decadent-tech <debosmitac8@gmail.com>
2025-09-09 22:12:06 +0530

    logistic_model

---
+++ logistic_model.py
@@ -0,0 +1,28 @@
+import nltk                         # NLP toolbox
+from os import getcwd
+import pandas as pd                 # Library for Dataframes 
+from nltk.corpus import twitter_samples 
+import matplotlib.pyplot as plt     # Library for visualization
+import numpy as np                  # Library for math functions
+
+from preprocessing import process_tweet
+from frequency import build_freqs # Our functions for NLP
+
+
+# select the set of positive and negative tweets
+all_positive_tweets = twitter_samples.strings('positive_tweets.json')
+all_negative_tweets = twitter_samples.strings('negative_tweets.json')
+
+tweets = all_positive_tweets + all_negative_tweets ## Concatenate the lists. 
+labels = np.append(np.ones((len(all_positive_tweets),1)), np.zeros((len(all_negative_tweets),1)), axis = 0)
+
+# split the data into two pieces, one for training and one for testing (validation set) 
+train_pos  = all_positive_tweets[:4000]
+train_neg  = all_negative_tweets[:4000]
+
+train_x = train_pos + train_neg 
+
+print("Number of tweets: ", len(train_x))
+
+data = pd.read_csv('logistic_features.csv'); # Load a 3 columns csv file using pandas function
+data.head(10) # Print the first three data entries
