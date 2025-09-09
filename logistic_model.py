91b03af585f39783b009499c800ae9c517b086fe
Decadent-tech <debosmitac8@gmail.com>
2025-09-09 22:25:37 +0530

    updates on logistic model

--- logistic_model.py
+++ logistic_model.py
@@ -25,4 +25,60 @@ train_x = train_pos + train_neg
 print("Number of tweets: ", len(train_x))
 
 data = pd.read_csv('logistic_features.csv'); # Load a 3 columns csv file using pandas function
-data.head(10) # Print the first three data entries
+print(data.head(10)) # Print the first three data entries
+
+# Each feature is labeled as bias, positive and negative
+X = data[['bias', 'positive', 'negative']].values # Get only the numerical values of the dataframe
+Y = data['sentiment'].values; # Put in Y the corresponding labels or sentiments
+
+print(X.shape) # Print the shape of the X part
+print(X) # Print some rows of X
+
+theta = [7e-08, 0.0005239, -0.00055517]
+
+# Plot the samples using columns 1 and 2 of the matrix
+fig, ax = plt.subplots(figsize = (8, 8))
+
+colors = ['red', 'green']
+
+# Color based on the sentiment Y
+ax.scatter(X[:,1], X[:,2], c=[colors[int(k)] for k in Y], s = 0.1)  # Plot a dot for each pair of words
+plt.xlabel("Positive")
+plt.ylabel("Negative")
+plt.title("Tweets represented in the feature space")
+plt.show()
+
+
+
+def neg(theta, pos):
+    return (-theta[0] - pos * theta[1]) / theta[2]
+
+
+def direction(theta, pos):
+    return    pos * theta[2] / theta[1]
+
+
+# Plot the samples using columns 1 and 2 of the matrix
+fig, ax = plt.subplots(figsize = (8, 8))
+
+colors = ['red', 'green']
+
+# Color base on the sentiment Y
+ax.scatter(X[:,1], X[:,2], c=[colors[int(k)] for k in Y], s = 0.1)  # Plot a dot for each pair of words
+plt.xlabel("Positive")
+plt.ylabel("Negative")
+
+# Now lets represent the logistic regression model in this chart. 
+maxpos = np.max(X[:,1])
+
+offset = 5000 # The pos value for the direction vectors origin
+
+# Plot a gray line that divides the 2 areas.
+ax.plot([0,  maxpos], [neg(theta, 0),   neg(theta, maxpos)], color = 'gray') 
+
+# Plot a green line pointing to the positive direction
+ax.arrow(offset, neg(theta, offset), offset, direction(theta, offset), head_width=500, head_length=500, fc='g', ec='g')
+# Plot a red line pointing to the negative direction
+ax.arrow(offset, neg(theta, offset), -offset, -direction(theta, offset), head_width=500, head_length=500, fc='r', ec='r')
+
+plt.show()
